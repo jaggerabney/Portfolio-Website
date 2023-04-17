50c935c6d683ba8444fb7c6337fa4f3896a3e2d5
jaggerabney <jagger.abney@gmail.com>
2023-04-17 16:26:59 -0700

    redid typewriter on my own

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import Navbar from "./components/Navbar/Navbar";
 import Heading from "./components/UI/Heading/Heading";
 
 import classes from "./App.module.css";
+import Typewriter from "./components/UI/Typewriter/Typewriter";
 
 function App() {
   return (
     <>
       <Navbar />
       <section className={classes.content}>
-        <Heading>Hey! I'm Jagger. Welcome to my website.</Heading>
+        <Typewriter text="Hey! I'm Jagger. Welcome to my website." />
       </section>
     </>
   );
