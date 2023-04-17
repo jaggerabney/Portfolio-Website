c0b889aa254be83476025c139f8d05de1231b739
jaggerabney <jagger.abney@gmail.com>
2023-04-17 15:39:35 -0700

    added typewriter effect to heading

--- frontend/src/components/UI/Heading/Heading.js
+++ frontend/src/components/UI/Heading/Heading.js
@@ -1,5 +1,21 @@
+import TypeWriterEffect from "react-typewriter-effect";
+
 import classes from "./Heading.module.css";
 
 export default function Heading(props) {
-  return <h1 className={classes.heading}>{props.children}</h1>;
+  return (
+    <div className={classes.wrapper}>
+      <TypeWriterEffect
+        textStyle={{
+          color: "#8516da",
+          fontSize: "48px",
+          fontWeight: 900,
+        }}
+        startDelay={500}
+        cursorColor={"#000000"}
+        text={"Hello! I'm Jagger. Welcome to my website."}
+        typeSpeed={50}
+      />
+    </div>
+  );
 }
