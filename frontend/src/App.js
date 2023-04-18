7b1a322683b1a9fab967980f0bcf40ed36034b96
jaggerabney <jagger.abney@gmail.com>
2023-04-17 17:27:58 -0700

    added some text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,17 @@ function App() {
       <Navbar />
       <section className={classes.content}>
         <Typewriter text="Hey! I'm Jagger. Welcome to my website." />
-        <CircularImage name="me_cropped.jpg" altText="Me!" />
+        <article className={classes.article}>
+          <CircularImage name="me_cropped.jpg" altText="Me!" />
+          <p>
+            I'm an aspiring fullstack developer from Seattle. I started coding
+            in the library of my middle school at 14, and I've since never
+            stopped. After earning my degree in 2021, I decided that I want to
+            program for the rest of my life. Nothing else on this earth
+            scratches my creative itch quite like it...and hey, it pays well,
+            too. Scroll down to see a little more about myself and my work!
+          </p>
+        </article>
       </section>
     </>
   );
