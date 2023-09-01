76afb0f2e7f2002620a43004f0da1b08a7b03557
jaggerabney <jagger.abney@gmail.com>
2023-08-31 18:17:24 -0700

    added BackgroundBlur component to nextjs version

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,18 +1,22 @@
 import Navbar from "../components/Navbar/Navbar";
 import BackgroundImage from "../components/BackgroundImage/BackgroundImage";
+import BackgroundBlur from "../components/BackgroundBlur/BackgroundBlur";
 import bgImage from "../public/images/Home.jpg";
+
+import "../styles/_app.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
+    <div className="app">
       <BackgroundImage
         src={bgImage}
         alt="Background"
         height={1080}
         width={1920}
       />
-    </>
+      <BackgroundBlur />
+      <Component {...pageProps} />
+      <Navbar />
+    </div>
   );
 }
